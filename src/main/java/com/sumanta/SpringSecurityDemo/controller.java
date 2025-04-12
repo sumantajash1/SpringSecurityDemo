4d49e4df409a804e6562d36910fe872420f6eaa7
sumanta <sumantajashh@gmail.com>
2025-04-12 15:45:08 +0530

    First Commit

---
+++ src/main/java/com/sumanta/SpringSecurityDemo/controller.java
@@ -0,0 +1,12 @@
+package com.sumanta.SpringSecurityDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello";
+    }
+}
